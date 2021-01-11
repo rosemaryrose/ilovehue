bfc919f82ee886439c75cdcfa80052eebe14dc5c
red-naxel-afk <rualex2018@yandex.ru>
2021-01-11 23:28:50 +0300

    "косметические" изменения, переименовая переменные, классы и т.д.

--- classes.py
+++ classes.py
@@ -2,14 +2,13 @@ import pygame
 import random
 
 
-class Board:
-    def __init__(self, col, s):
-        self.width = s[0]
-        self.height = s[1]
-        self.board = self.make_pole(col, s)
-        self.random_board = self.random_color_mix(col)
-        # for i in self.board:
-        # print(i)
+class ILoveHueGame:
+    def __init__(self, corner_colors, numbers_of_cells):
+        self.width = numbers_of_cells[0]
+        self.height = numbers_of_cells[1]
+        self.board = self.make_board(corner_colors, numbers_of_cells)
+        self.random_board = self.random_color_mix(corner_colors)
+
         self.top = 10
         self.cell_size = 30
 
@@ -17,6 +16,7 @@ class Board:
         self.top = top
         self.cell_size = cell_size
         size = self.width * self.cell_size, self.top * 2 + self.height * self.cell_size
+
         return pygame.display.set_mode(size)
 
     def render(self, screen):
@@ -34,28 +34,28 @@ class Board:
                             self.top + self.cell_size * (self.height - 1) + self.cell_size // 2), 3)
 
     # создание поля
-    def make_pole(self, colors, size):
-        sp1 = self.make_sp(colors[0], colors[1], size[0])
-        sp2 = self.make_sp(colors[2], colors[3], size[0])
-        sp0 = []
-        # print(sp1, sp2)
-        for i in range(size[0]):
-            sp0.append(self.make_sp(sp1[i], sp2[i], size[1]))
-        return sp0
+    def make_board(self, corner_colors, numbers_of_cells):
+        line1 = self.make_line(corner_colors[0], corner_colors[1], numbers_of_cells[0])
+        line2 = self.make_line(corner_colors[2], corner_colors[3], numbers_of_cells[0])
+        board = []
+
+        for i in range(self.width):
+            board.append(self.make_line(line1[i], line2[i], numbers_of_cells[1]))
+        return board
 
     # генерация цветового поля
-    def make_sp(self, col1, col2, a1):
-        sp = [[0, 0, 0] for i in range(a1)]
+    def make_line(self, start_color, end_color, cells_width):
+        line = [[0, 0, 0] for _ in range(cells_width)]
         for i in range(3):
-            s0 = col1[i]
-            s1, s2 = col1[i], col2[i]
-            k = (s2 - s1) / (a1 - 1)
-            for j in range(1, a1):
-                s1 += k
-                sp[j][i] = int(s1)
-            sp[0][i] = s0
-            sp[a1 - 1][i] = s2
-        return sp
+            color0 = start_color[i]
+            color1, color2 = start_color[i], end_color[i]
+            k = (color2 - color1) / (cells_width - 1)
+            for j in range(1, cells_width):
+                color1 += k
+                line[j][i] = int(color1)
+            line[0][i] = color0
+            line[cells_width - 1][i] = color2
+        return line
 
     def random_color_mix(self, corner_colors):
         random_mix = self.board.copy()
@@ -64,7 +64,7 @@ class Board:
             for cell in column:
                 if tuple(cell) not in corner_colors:
                     cells.append(cell)
-        # print(cells)
+
         for i in range(self.width):
             for j in range(self.height):
                 if tuple(random_mix[i][j]) not in corner_colors:
@@ -72,5 +72,5 @@ class Board:
         return random_mix
 
 
-a = [(255, 165, 0), (50, 205, 50), (255, 248, 220), (128, 0, 0)]  # цвета клеток
-b = [10, 15]  # рамеры поля
+class ILoveHueMenu:
+    pass
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from classes import *
 
+corner_colors = [(255, 165, 0), (50, 205, 50), (255, 248, 220), (128, 0, 0)]  # цвета клеток
+size = [10, 15]  # рамеры поля
+
 if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('ILoveHue')
 
-    board = Board(a, b)
+    board = ILoveHueGame(corner_colors, size)
     screen = board.set_view(30, 40)
+
     running = True
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+        # отрисовка следующего кадра
         screen.fill((0, 0, 0))
         board.render(screen)
         pygame.display.flip()
