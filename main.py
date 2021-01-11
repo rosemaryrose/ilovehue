72ddb2bb1698c73b1f6fcc6a1785ba1dc0fa48d7
rosemaryrose <74919488+rosemaryrose@users.noreply.github.com>
2021-01-12 01:05:19 +0300

    New commit
    Изменения, связанные с изменениями в classes.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from classes import *
 
 corner_colors = [(255, 165, 0), (50, 205, 50), (255, 248, 220), (128, 0, 0)]  # цвета клеток
-size = [10, 15]  # рамеры поля
+size = [10, 15]  # размеры поля
 
 if __name__ == '__main__':
     pygame.init()
@@ -9,14 +9,13 @@ if __name__ == '__main__':
 
     board = ILoveHueGame(corner_colors, size)
     screen = board.set_view(30, 40)
-
     running = True
-
+    board.render_start(screen)
+    pygame.display.flip()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
         # отрисовка следующего кадра
         screen.fill((0, 0, 0))
         board.render(screen)
