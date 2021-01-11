bcd0e03781118895f61e27df7969dc56ee4529f9
Полина <lovepolina2004@gmail.com>
2021-01-11 20:04:29 +0300

    Рандомное перемешение клеточек и точки-ограничители

--- first.py
+++ first.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 
 class Board:
@@ -6,26 +7,38 @@ class Board:
         self.width = s[0]
         self.height = s[1]
         self.board = self.make_pole(col, s)
+        self.random_board = self.random_color_mix(col)
+        # for i in self.board:
+        # print(i)
         self.top = 10
-        self.cs = 30
+        self.cell_size = 30
 
     def set_view(self, top, cell_size):
         self.top = top
-        self.cs = cell_size
-        size = self.width * self.cs, self.top * 2 + self.height * self.cs
+        self.cell_size = cell_size
+        size = self.width * self.cell_size, self.top * 2 + self.height * self.cell_size
         return pygame.display.set_mode(size)
 
     def render(self, screen):
         for i in range(self.width):
             for j in range(self.height):
-                pygame.draw.rect(screen, self.board[i][j], ((i * self.cs, self.top + j * self.cs), (self.cs, self.cs)))
+                pygame.draw.rect(screen, self.board[i][j], ((i * self.cell_size, self.top + j * self.cell_size),
+                                                            (self.cell_size, self.cell_size)))
+        pygame.draw.circle(screen, (0, 0, 0), (self.cell_size // 2, self.top + self.cell_size // 2), 3)
+        pygame.draw.circle(screen, (0, 0, 0),
+                           (self.cell_size // 2 + self.cell_size * (self.width - 1), self.top + self.cell_size // 2), 3)
+        pygame.draw.circle(screen, (0, 0, 0), (self.cell_size // 2,
+                                               self.top + self.cell_size * (self.height - 1) + self.cell_size // 2), 3)
+        pygame.draw.circle(screen, (0, 0, 0),
+                           (self.cell_size // 2 + self.cell_size * (self.width - 1),
+                            self.top + self.cell_size * (self.height - 1) + self.cell_size // 2), 3)
 
     # создание поля
     def make_pole(self, colors, size):
         sp1 = self.make_sp(colors[0], colors[1], size[0])
         sp2 = self.make_sp(colors[2], colors[3], size[0])
         sp0 = []
-        print(sp1, sp2)
+        # print(sp1, sp2)
         for i in range(size[0]):
             sp0.append(self.make_sp(sp1[i], sp2[i], size[1]))
         return sp0
@@ -44,9 +57,23 @@ class Board:
             sp[a1 - 1][i] = s2
         return sp
 
+    def random_color_mix(self, corner_colors):
+        random_mix = self.board.copy()
+        cells = list()
+        for column in self.board:
+            for cell in column:
+                if tuple(cell) not in corner_colors:
+                    cells.append(cell)
+        # print(cells)
+        for i in range(self.width):
+            for j in range(self.height):
+                if tuple(random_mix[i][j]) not in corner_colors:
+                    random_mix[i][j] = cells.pop(random.randrange(0, len(cells)))
+        return random_mix
+
 
 a = [(255, 165, 0), (50, 205, 50), (255, 248, 220), (128, 0, 0)]  # цвета клеток
-b = [10, 15]  # рамеры клеток
+b = [10, 15]  # рамеры поля
 
 if __name__ == '__main__':
     pygame.init()
@@ -63,4 +90,3 @@ if __name__ == '__main__':
         board.render(screen)
         pygame.display.flip()
     pygame.quit()
-
